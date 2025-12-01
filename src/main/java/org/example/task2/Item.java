4f2a1da40b7c7ab07abe17767dac300ff5de5b2b
vladaguniya <9100299@stud.op.edu.ua>
2025-12-01 23:10:23 +0200

    Update Item.java

--- src/main/java/org/example/task2/Item.java
+++ src/main/java/org/example/task2/Item.java
@@ -2,22 +2,34 @@ package org.example.task2;
 
 public class Item {
 
-    public long id;
-    public String name;
-    public double price;
+    private final long id;
+    private final String name;
+    private final double price;
 
-    public Item(long _id, String _name, double _price) {
-        this.id = _id;
-        this.name = _name;
-        this.price = _price;
+    public Item(long id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getPrice() {
+        return this.price;
     }
 
     @Override
     public String toString() {
         return "Item{" +
-                "id=" + id +
-                ", price=" + price +
-                ", name='" + name + '\'' +
+                "id=" + this.id +
+                ", price=" + this.price +
+                ", name='" + this.name + '\'' +
                 '}' + "\n";
     }
 }
